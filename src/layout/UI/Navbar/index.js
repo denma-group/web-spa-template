0926d9e51231ce5673fc54eeb60c86d243ecb23e
Robert <rmolinamir@gmail.com>
2019-05-06 00:46:24 -0400

    Initial Navbar component.

--- src/layout/UI/Navbar/index.js
+++ src/layout/UI/Navbar/index.js
@@ -1,6 +1,8 @@
+// Libraries
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+// JSX
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,37 +10,35 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const styles = {
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+const StyledAppBar = styled(AppBar)`
+  && {
+    color: ${props => props.theme.whiteColor};
+    background-color: ${props => props.theme.darkColor};
+    box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            Denma
-          </Typography>
-          <Button color="inherit">Contact us</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
+    h6 {
+      flex-grow: 1;
+    }
 
-ButtonAppBar.propTypes = {
-  classes: PropTypes.instanceOf(Object).isRequired
-};
+    button {
+      margin-left: -12;
+      margin-right: 20;
+    }
+  }
+`;
 
-export default withStyles(styles)(ButtonAppBar);
+const Navbar = () => (
+  <StyledAppBar position="static">
+    <Toolbar>
+      <IconButton color="inherit" aria-label="Menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" color="inherit">
+        Denma
+      </Typography>
+      <Button color="inherit">Contact us</Button>
+    </Toolbar>
+  </StyledAppBar>
+);
+
+export default (Navbar);
